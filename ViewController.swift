143b12aa6f488db996f6ed75c73d247781cf4560
Montana <montana@Montanas-MacBook-Pro.local>
2020-08-18 21:44:09 -0400

    adding files

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Squid Squad
+//
+//  Created by Montana  on 8/18/20.
+//  Copyright © 2020 Montana Braswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
